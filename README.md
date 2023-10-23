b049e9a17097bf4d97af3ba17f0e90207fda8908
Josemi Toribio <josemitoribio@gmail.com>
2023-10-23 13:22:48 +0200

    Laboratorio 03 finalizado

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-# typescript-sandbox
+# Laboratorio 03
 
-Para la parte de cada módulo en la que te planteamos ejercicios con JavaScript puedes usar este sandbox.
+## **Variables**
 
-Pasos:
+### **Grupos Musicales**
+Queremos mostrar información acerca de grupos musicales.
 
-- Clonate el proyecto.
-- Instala las dependencias con `npm install`.
-- Ejecuta el sandbox con `npm run dev`.
-- Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
-- Puedes empezar a meter tu código en el fichero:
+Si estás trabajando con TypeScript habría que crear un interfaz para representar a un grupo musical.
+
+La información de los grupos que queremos mostrar:
+
+- Nombre del grupo / cantante / compositor (string).
+- Año de fundación: cuando se formó el grupo (numero).
+- Si está en activo (booleano).
+- Género musical (string).
+
+Cada género queremos tenerlo en una variable.
+
+Los grupos que vamos a mostrar:
+
+- The Beatles / 1960 / Activo: true / 🎵 Pop Rock
+- Queen / 1970 / Activo: false / 🎸 Rock
+- AC DC / 1973 / Activo: true / 🤘 Hard Rock
+- Ludwig van Beethoven / 1770 / Activo: false / 🎼 Clásica
+- The Rolling Stones / 1962 / Activo: true / 🎸 Rock
+
+Queremos mostrar cada grupo por consola, el nombre del grupo de música queremos ponerlo en negrita, con fuente más grande y color de fondo verde.
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,67 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+//Módulo 3 - Variables - Laboratorio
+
+interface GruposMusicales {
+    nombre: string;
+    año: number;
+    activo: boolean;
+    genero: string;
+}
+
+const popRock: string = "🎵 Pop Rock";
+const rock: string = "🎸 Rock";
+const hardRock: string = "🤘 Hard Rock";
+const clasica: string = "🎼 Clásica";
+
+const grupoA: GruposMusicales = {
+    nombre: "The Beatles",
+    año: 1960,
+    activo: true,
+    genero: popRock,
+};
+
+const grupoB: GruposMusicales = {
+    nombre: "Queen",
+    año: 1970,
+    activo: false,
+    genero: rock,
+};
+
+const grupoC: GruposMusicales = {
+    nombre: "AC DC",
+    año: 1973,
+    activo: true,
+    genero: hardRock,
+};
+
+const grupoD: GruposMusicales = {
+    nombre: "Ludwig van Beethoven",
+    año: 1770,
+    activo: false,
+    genero: clasica,
+};
+
+const grupoE: GruposMusicales = {
+    nombre: "The Rolling Stones",
+    año: 1962,
+    activo: true,
+    genero: rock,
+};
+
+const estiloTitulo: string = "font-weight:800; font-size:15px; background-color:green;";
+
+//Grupo A
+console.log (`%c${grupoA.nombre}`, estiloTitulo,`/ ${grupoA.año} / ${grupoA.activo} / ${grupoA.genero}`);
+
+//Grupo B
+console.log (`%c${grupoB.nombre}`, estiloTitulo, `/ ${grupoB.año} / ${grupoB.activo} / ${grupoB.genero}`);
+
+//Grupo C
+console.log (`%c${grupoC.nombre}`, estiloTitulo, `/ ${grupoC.año} / ${grupoC.activo} / ${grupoC.genero}`);
+
+//Grupo D
+console.log (`%c${grupoD.nombre}`, estiloTitulo, `/ ${grupoD.año} / ${grupoD.activo} / ${grupoD.genero}`);
+
+//Grupo E
+console.log (`%c${grupoE.nombre}`, estiloTitulo, `/ ${grupoE.año} / ${grupoE.activo} / ${grupoE.genero}`);
